452f1d391f0ba2038468104e79a2da34db6a2f37
Navicstein <navicsteinrotciv@gmail.com>
2024-04-18 23:53:24 +0100

    💪 fix: issue with bigints for telegram

--- app/db/models.py
+++ app/db/models.py
@@ -1,14 +1,17 @@
 import datetime
-from sqlalchemy import Column, DateTime, func
+from sqlalchemy import BigInteger, Column, DateTime, Numeric, func
 from sqlmodel import SQLModel, Field
 from telegram import User as TelegramUser
 
 
 class User(SQLModel, table=True):
     __tablename__ = "users"  # type: ignore
-    id: int = Field(default=None, primary_key=True)
+    id: int = Field(
+        default=None, sa_column=Column(Numeric(), primary_key=True, autoincrement=True)
+    )
+    telegram_id: int = Field(default=None, sa_column=Column(Numeric()))
+
     full_name: str
-    telegram_id: int
 
     is_premium: bool = Field(default=False)
 
@@ -23,11 +26,17 @@ class User(SQLModel, table=True):
         default_factory=datetime.datetime.utcnow,
     )
 
+    @property
+    def real_id(self):
+        return int(self.telegram_id)
+
 
 class Chat(SQLModel, table=True):
     __tablename__ = "chats"  # type: ignore
-    id: int = Field(default=None, primary_key=True)
-    user_id: int
+    id: int = Field(
+        default=None, sa_column=Column(Numeric(), primary_key=True, autoincrement=True)
+    )
+    user_id: int = Field(default=None, sa_column=Column(Numeric(), primary_key=True))
     role: str
     text: str
     created_at: datetime.datetime | None = Field(
